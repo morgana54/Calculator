d0dd5a4cfa6d195d7d9a8d17100457b5958bf33d
Jakub Pietrzak <jpietrzi@gmail.com>
2021-03-02 13:14:22 +0100

    Setup envoirement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-// posiłkuj się tym w razie czego: https://www.freecodecamp.org/news/how-to-build-a-markdown-previewer-with-react-js/
-
 class App extends React.Component{
 render(){
   return (
